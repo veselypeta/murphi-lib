b38fccf1fffc9ae314890abcd71c6df16d2ffbb0
initial <veselypeta@gmail.com>
2021-04-07 13:58:52 +0100

    remove unused assert

--- src/models/TypeDecl.cpp
+++ src/models/TypeDecl.cpp
@@ -4,7 +4,6 @@
 namespace murphi
 {
     std::string TypeDecl::getAsString(){
-        assert(expr->getAsString() != "" && "somehow got empty string!");
         return id + " : " + expr->getAsString() + ";";
     }
 } // namespace murphi
