d746c5f8974388d0bc56193941e8990b13ffd5a0
initial <veselypeta@gmail.com>
2021-04-08 10:25:19 +0100

    procedure basic - todo stmts

---
+++ include/proc/ProcDecl.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "interfaces/Identifyable.h"
+#include "interfaces/Printable.h"
+#include "models/Decl.h"
+#include "models/TypeExpr.h"
+#include <string>
+
+namespace murphi {
+namespace proc {
+
+class ProcDecl : public Printable<ProcDecl>, public Identifyable<ProcDecl> {
+public:
+  ProcDecl() {}
+  virtual ~ProcDecl() {}
+  virtual std::string getAsString() = 0;
+  virtual std::string getId() = 0;
+};
+
+class Function : public ProcDecl {
+public:
+  Function() {}
+  ~Function() { delete rv; }
+  virtual std::string getAsString();
+  virtual std::string getId();
+
+private:
+  std::string id;
+  TypeExpr *rv;
+  Decl forwardDecls;
+  // stmts
+};
+
+class Procedure : public ProcDecl {
+public:
+  virtual std::string getAsString();
+  virtual std::string getId();
+
+private:
+  std::string id;
+  Decl forwardDecls;
+  // stmts
+};
+
+} // namespace proc
+} // namespace murphi
---
+++ src/proc/ProcDecl.cpp
@@ -0,0 +1,22 @@
+#include "proc/ProcDecl.h"
+
+namespace murphi {
+namespace proc {
+
+std::string Function::getId() { return id; }
+
+std::string Function::getAsString() {
+  return "function " + getId() + "(" /* parameters*/ +
+         ") : " + rv->getAsString() + ";" + forwardDecls.getAsString() +
+         "begin" /*smts*/ + "end;";
+}
+
+std::string Procedure::getId() { return id; }
+
+std::string Procedure::getAsString() {
+  return "procedure " + getId() + "(" /*parameters*/ + ");" +
+         forwardDecls.getAsString() + "begin" /*stmts*/ + "end;";
+}
+
+} // namespace proc
+} // namespace murphi
